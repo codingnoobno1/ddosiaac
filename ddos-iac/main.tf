8dcd8a80db6fbdbc6c9a1532dbb485cc8052ee44
codingnoobno1 <164862821+codingnoobno1@users.noreply.github.com>
2025-07-22 12:19:17 +0530

    Update main.tf

--- ddos-iac/main.tf
+++ ddos-iac/main.tf
@@ -11,17 +11,15 @@ terraform {
 
 provider "azurerm" {
   features {}
-   skip_provider_registration = true
+  skip_provider_registration = true
 }
 
-# Call cloud_infra module
 module "cloud" {
-  source = "./cloud_infra"
-
+  source              = "./cloud_infra"
   resource_group_name = "ddos-rg"
   location            = "East US"
   vnet_name           = "ddos-vnet"
   address_space       = ["10.0.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
-  subnet_names        = ["web", "attacker"]
+  subnet_names        = ["web", "monitoring", "attacker"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
